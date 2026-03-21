d65b503cbea96195f9612f87db14cac63b9a1f47
Natnael Nigatu <natnaelnigatu23@gmail.com>
2026-03-21 13:55:11 +0300

    add missing Spring Boot context test from initial project setup

---
+++ src/test/java/com/ctbe/Natnaelnigatu/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctbe.Natnaelnigatu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
