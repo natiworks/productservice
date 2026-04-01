c7fb4f260915e390ddac830b27316762e6510101
Natnael Nigatu <natnaelnigatu23@gmail.com>
2026-04-01 19:03:12 +0300

    feat(repo): add findByCategory and findByNameContainingIgnoreCase

--- src/main/java/com/ctbe/Natnaelnigatu/repository/ProductRepository.java
+++ src/main/java/com/ctbe/Natnaelnigatu/repository/ProductRepository.java
@@ -8,7 +8,11 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    // Spring Data generates: SELECT * FROM products WHERE LOWER(name) LIKE %?%
 
     List<Product> findByNameContainingIgnoreCase(String keyword);
 
+    // Spring Data generates: SELECT * FROM products WHERE category = ?
+
+    List<Product> findByCategory(String category);
 }
